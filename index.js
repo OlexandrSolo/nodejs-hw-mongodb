4404ba415483541d59c9003eb6d7701cb129194c
Your Name <you@example.com>
2024-10-30 07:34:58 +0200

    create web server

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-import express from 'express';
-import cors from "cors"
-import pino from "pino-http"
-import { contacts } from "./src/db/contacts.js"
+import { startServer } from "./src/server.js";
 
-const app = express();
+const bootstrap = async () => {
+    startServer()
+}
 
-app.use((req, res, next) => {
-    console.log("first middleware");
-    next()
-})
-
-app.get("/", (req, res) => {
-    res.json([])
-})
-
-app.get('/products', (req, res) => {
-    res.json([])
-})
-
-app.get("/contacts", (req, res) => {
-    res.json(contacts)
-})
-
-app.listen(3000, () => console.log("Server running on 3000 port"))
+bootstrap()
