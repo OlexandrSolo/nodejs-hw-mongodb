7d13d65af1278497b4a9d5560e4811e3b12851c7
Your Name <you@example.com>
2024-11-29 22:08:23 +0200

    edited

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,3 +1,5 @@
+import createHttpError from "http-errors";
+
 export const errorHandler = (error, req, res, next) => {
     throw createHttpError(500, "Something went wrong")
     // const { status = 500, messages = "Server error" } = error;
