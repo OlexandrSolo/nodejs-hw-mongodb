b703135d4b630ea1dd7800a6ebc70fdb99aed35e
Your Name <you@example.com>
2024-11-30 06:49:12 +0200

    add new lib

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,10 +1,10 @@
 import createHttpError from "http-errors";
 
 export const errorHandler = (error, req, res, next) => {
-    throw createHttpError(500, "Something went wrong")
-    // const { status = 500, messages = "Server error" } = error;
-    // res.status(status).json({
-    //     status,
-    //     messages,
-    // })
+    // throw createHttpError(500, "Something went wrong")
+    const { status = 500, messages = "Server error" } = error;
+    res.status(status).json({
+        status,
+        messages,
+    })
 }
--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,9 +1,9 @@
 import createHttpError from "http-errors";
 
 export const notFoundHandler = (req, res) => {
-    throw createHttpError(404, "Route not found")
-    // res.status(404).json({
-    //     status: 404,
-    //     messages: "Not Found"
-    // })
+    // throw createHttpError(404, "Route not found")
+    res.status(404).json({
+        status: 404,
+        messages: "Not Found"
+    })
 }
