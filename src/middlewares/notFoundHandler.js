c8d0dd432ad2ff392eceff5c9ce693f97edfb44e
Your Name <you@example.com>
2024-12-03 07:11:40 +0200

    fix

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,13 +1,10 @@
 import createHttpError from "http-errors"
 
 export const notFoundHandler = (req, res) => {
-<<<<<<< HEAD
     // throw createHttpError(404, "Route not found")
+    // throw createHttpError(404, `Route ${req.url} not found`);
     res.status(404).json({
         status: 404,
         messages: "Not Found"
     })
-=======
-    throw createHttpError(404, `Route ${req.url} not found`);
->>>>>>> hw3-crud
 }
