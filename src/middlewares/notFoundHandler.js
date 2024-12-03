3e564eb1032553c3e39842c94e5c953cf97562ce
Your Name <you@example.com>
2024-12-03 07:14:19 +0200

    fix

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,5 +1,8 @@
 import createHttpError from "http-errors"
 
 export const notFoundHandler = (req, res) => {
-    throw createHttpError(404, `Route ${req.url} not found`);
+    res.status(404).json({
+        status: 404,
+        messages: "Not Found"
+    })
 }
